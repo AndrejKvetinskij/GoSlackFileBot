8286f5d33e8a72694a9ade9866ba96af197e8309
AndrejKvetinskij <kvetinskij.andrej@mail.ru>
2023-05-10 16:09:00 +0300

    go-slack-file-bot

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5238373075283-5241046913362-eskTBILbhj8H8zqZFUWWiTe3")
+	os.Setenv("CHANNEL_ID", "C056XGJ4TDK")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"price_RB.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+	}
+}
